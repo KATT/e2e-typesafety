c5d1240fef6e00c3620cabd5223e3ccc55b896a6
KATT <alexander@n1s.se>
2020-10-19 21:37:27 +0200

    🎨 Convert to typescript

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,7 +1,8 @@
+import { AppProps } from 'next/app'
 import { ApolloProvider } from '@apollo/client'
 import { useApollo } from '../apollo/client'
 
-export default function App({ Component, pageProps }) {
+export default function App({ Component, pageProps }: AppProps) {
   const apolloClient = useApollo(pageProps.initialApolloState)
 
   return (
