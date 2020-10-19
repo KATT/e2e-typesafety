ce31e435645b72ff7b029577268d840f0263f40a
KATT <alexander@n1s.se>
2020-10-19 22:01:35 +0200

    🎨 Replace Apollo with nexus-schema

--- apollo/schema.ts
+++ apollo/schema.ts
@@ -1,8 +1,35 @@
-import { makeExecutableSchema } from 'graphql-tools'
-import { typeDefs } from './type-defs'
-import { resolvers } from './resolvers'
+import { queryType, stringArg, makeSchema, objectType } from "@nexus/schema";
 
-export const schema = makeExecutableSchema({
-  typeDefs,
-  resolvers,
+
+const User = objectType({
+  name: "Viewer", 
+  definition(t) {
+    t.id("id")
+    t.string("name")
+    t.string("status")
+  }
 })
+
+const Query = queryType({
+  definition(t) {
+    t.field('viewer', {
+      type: "Viewer",
+      resolve() {
+        return { 
+          id: 'b6a2ab5b-ad39-445a-b56f-b0ec9cb20549', 
+          name: 'John Smith', 
+          status: 'cached',
+        }
+      }
+    })
+  },
+});
+
+export const schema = makeSchema({
+  types: [Query, User],
+  shouldGenerateArtifacts: process.env.NODE_ENV === 'development',
+  outputs: {
+    schema: process.cwd() + "/__generated__/schema.graphql",
+    typegen: process.cwd() + "/__generated__/typings.ts",
+  },
+});
